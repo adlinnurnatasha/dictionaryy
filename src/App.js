8ce2d99a4e979dfcde74495f9f22d1bd7ca47e41
Rusydiah-BeSquare <75686002+rsydh@users.noreply.github.com>
2021-10-20 14:10:03 +0800

    Revert "HOHO"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Navbar from "./Components/Navbar";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/Components/Footer.js
+++
@@ -1,7 +0,0 @@
-import "./Footer.css";
-
-const Footer = () => {
-  return <footer className="footer"> &copy; 2021 One Dictionary </footer>;
-};
-
-export default Footer;
