03882b31013aa3cb4d1a1ba3c1617cc8a1a61d0d
Suhaila Japar <78157287+suhailajapar@users.noreply.github.com>
2021-10-21 21:51:15 +0800

    Backup API & Style Update (#14)
    * style: home (769px)
    
    * add btn:hover style
    
    * fix lol
    
    * touch-up
    
    * Styling updated & backup API added

--- src/Components/RandomWord.js
+++ src/Components/RandomWord.js
@@ -4,11 +4,19 @@ import "./RandomWord.css";
 const RandomWord = () => {
   const [randomWord, setRandomWord] = useState([]);
 
+  //The Actual API
   useEffect(() => {
     fetch("https://random-words-api.vercel.app/word")
       .then((result) => result.json())
       .then((data) => setRandomWord(data));
   }, []);
+
+  //Backup API - Words only
+  // useEffect(() => {
+  //   fetch("https://random-word-api.herokuapp.com/word?number=1")
+  //     .then((result) => result.json())
+  //     .then((data) => setRandomWord(data));
+  // }, []);
 
   return (
     <div className="wotd">
@@ -19,6 +27,7 @@ const RandomWord = () => {
             <div>
               <div className="theWord">"{r.word}"</div>
               <div className="definition">{r.definition}</div>
+              {/* <div className="theWord">"{r}"</div> */}
             </div>
           );
         })}
